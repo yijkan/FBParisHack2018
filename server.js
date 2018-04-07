0baf5e546149ebf138021d2326027a5ca36d77dd
Yijin Kang <yijinkang@gmail.com>
2018-04-08 01:20:27 +0200

    handling multiple posts

--- server.js
+++ server.js
@@ -107,12 +107,45 @@ const server = https.createServer(httpsOptions, app).listen(port, () => {
 
 let analyse_text = function (result) {
   let watson_text = require('./watson-test.js');
-  let message = [];
+  let messages = [];
   for (let i in result.data) {
-    message.push(result.data[i].message);
+    messages.push(result.data[i].message);
   }
+  console.log(messages);
   watson_text = watson_text.analyse();
-  let final_result = watson_text(message[2]);
+  let results = [];
+  let promise;
+  for (let i in messages) {
+    if (i == 0) {
+      promise = watson_text(messages[0]);
+    } else {
+      promise = promise.then(
+          function(result) {
+          console.log(result);
+          results.push(result);
+          return watson_text(messages[i]);
+        },
+        function(error) {
+          print("error");
+        }
+      );
+    }
+  }
+  if (promise) {
+    promise.then(
+      function(result) {
+        console.log(result);
+        results.push(result);
+        var stmt = mk_matlab_stmt(results);
+        // var response = execSync('matlab -nodisplay -nojvm -nosplash -nodesktop -r "' + stmt + '"');
+        var response = execSync('echo "' + stmt + '"');
+        console.log(response.toString()); // prints echoed text to console
+      },
+      function(error) {
+        print("error");
+      }
+    );
+  }
 
   // const request = require('request-promise');
   // const user = require('./config.js');
@@ -136,4 +169,28 @@ let analyse_text = function (result) {
   //   let final_result = watson_text(message[2]);
   //   console.log(message[2]);
   // })
+}
+
+function len_str(word_lengths) {
+    let str = "";
+    for (let i in word_lengths) {
+        str = " " +  str + word_lengths[i];
+    }
+    str = str + "]";
+    return str;
+}
+
+function mk_list_item(obj) {
+    let str = "[" + obj.emotions.sadness + " " + obj.emotions.joy + " " + obj.emotions.fear + " ";
+    str = str + obj.emotions.disgust + " " + obj.emotions.anger + " " + obj.sentiment;
+    str = str + len_str(obj.word_lengths);
+    return str;
+}
+
+function mk_matlab_stmt(obj_array) {
+    let str = "[";
+    for (let i in obj_array) {
+        str = str + mk_list_item(obj_array[i]) + " ";
+    }
+    return str + "]";
 }
